3a223060a69ad7d31dab533f15dce506f93f2bad
alper.gurpinar <alper.gurpinar@teksenbilisim.com>
2022-09-14 17:46:10 +0300

    geçici update

--- resources/js/Views/Query/index.js
+++ resources/js/Views/Query/index.js
@@ -1,71 +1,94 @@
-import { inject, observer } from 'mobx-react';
-import React,{ useEffect , useState} from 'react';
-import Layout from '../../Components/Layout/homeLayout';
-import DataTable from 'react-data-table-component';
-import SubHeaderComponent from '../../Components/Form/SubHeaderComponent';
-import ExpandedComponent from '../../Components/Form/ExpandedComponent';
-import swal from 'sweetalert';
+import { inject, observer } from "mobx-react";
+import React, { useEffect, useState } from "react";
+import Layout from "../../Components/Layout/homeLayout";
+import DataTable from "react-data-table-component";
+import SubHeaderComponent from "../../Components/Form/SubHeaderComponent";
+import ExpandedComponent from "../../Components/Form/ExpandedComponent";
+import swal from "sweetalert";
 const Index = (props) => {
-    const [data,setData] = useState([]);
-    const [refresh,setRefresh] = useState(false);
-    const [filter,setFilter] = useState({
-        filteredData:[],
-        text:'',
-        isFilter:false
+    const [data, setData] = useState([]);
+    const [refresh, setRefresh] = useState(false);
+    const [filter, setFilter] = useState({
+        filteredData: [],
+        text: "",
+        isFilter: false,
     });
 
     useEffect(() => {
-        axios.get(`/api/query`,{
-            headers:{
-                Authorization: 'Bearer '+ props.AuthStore.appState.user.access_token
-            }
-        }).then((res) => {
-           setData(res.data.data);
-        })
-        .catch(e => console.log(e));
-    },[refresh]);
-
-
+        axios
+            .get(`/api/query`, {
+                headers: {
+                    Authorization:
+                        "Bearer " + props.AuthStore.appState.user.access_token,
+                },
+            })
+            .then((res) => {
+                setData(res.data.data);
+            })
+            .catch((e) => console.log(e));
+    }, [refresh]);
 
 
     return (
         <Layout>
-
             <div className="container">
                 <div className="row">
-                    <div className="col-md-12">
+                    <div className="col-md-12" style={{ overflow: "hidden" }}>
                         <DataTable
-                            columns={
-                                [
-                                    {
-                                        name: 'Sorgu Adı',
-                                        selector:'name'
+                            columns={[
+                                {
+                                    name: "Sorgu Adı",
+                                    selector: "name",
+                                    width: "333px",
+                                    style: {
+                                        overflow: "hidden",
                                     },
-                                    {
-                                        name: 'Kısa Kod',
-                                        selector:'code'
+                                },
+                                {
+                                    name: "Kısa Kod",
+                                    width: "333px",
+                                    selector: "code",
+                                    style: {
+                                        overflow: "hidden",
                                     },
-                                    {
-                                        name: 'Sorgu',
-                                        selector:'sqlQuery'
+                                },
+                                {
+                                    width: "920px",
+                                    name: "Sorgu",
+                                    selector: "sqlQuery",
+                                    style: {
+                                        overflow: "hidden",
                                     },
-                                ]
-                            }
+                                },
+                            ]}
+                            allowOverflow={false}
                             subHeader={true}
                             responsive={true}
                             hover={true}
                             fixedHeader
                             pagination
                             expandableRows
-                            expandableRowsComponent={<ExpandedComponent/>}
-                            data={(filter.isFilter) ? filter.filteredData : data}
-                            subHeaderComponent={<SubHeaderComponent action ={{ class:'btn btn-success',uri:() => props.history.push('/sorgular/ekle'),title:'Parametre Ekle'}} />}
+                            expandableRowsComponent={
+                                <ExpandedComponent data={data} />
+                            }
+                            data={data}
+                            subHeaderComponent={
+                                <SubHeaderComponent
+                                    action={{
+                                        class: "btn btn-success",
+                                        uri: () =>
+                                            props.history.push(
+                                                "/sorgular/ekle"
+                                            ),
+                                        title: "Sorgu Ekle",
+                                    }}
+                                />
+                            }
                         />
                     </div>
                 </div>
             </div>
-
         </Layout>
-    )
+    );
 };
 export default inject("AuthStore")(observer(Index));
