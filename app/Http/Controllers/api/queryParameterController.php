678541b6deb303d4c011bc3e8902d541a768413b
Hakan küçüksert <hakan.kucuksert@teksenbilisim.com>
2022-09-16 10:25:47 +0300

    sorgu parametreleri düzenleme ve detay apileri eklendi

--- app/Http/Controllers/api/queryParameterController.php
+++ app/Http/Controllers/api/queryParameterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\QueryParameter;
 use App\Models\UserHasRole;
+use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 class queryParameterController extends Controller
@@ -55,12 +56,30 @@ class queryParameterController extends Controller
             ]);
         }
     }
-    public function show($id){}
+    public function show($id){
+        $queryParameter = QueryParameter::where('id',$id)->first();
+        return response()->json([
+            'success'=>true,
+            'query'=>$queryParameter
+        ]);
+    }
 
     public function edit($id){}
 
-    public function update(Request $request, $id){}
-
+    public function update(Request $request, $id){
+        $user = request()->user();
+        $roleId = UserHasRole::where('user_id',$user->id)->first();
+        $role = Role::where('id',$roleId->role_id)->first();
+        $control = QueryParameter::where('id',$id)->count();
+        if($role->code != 'superAdmin') { return response()->json(['success'=>false,'message'=>'Sorgu Parametresi Güncellenemedi, Yetkiniz bulunmamaktadır']);}
+        $update = QueryParameter::where('id',$id)->update([
+            'parameter'=>$request->parameter,
+            'name'=>$request->name
+        ]);
+        if($update){
+            return response()->json(['success'=>true,'message'=>'Sorgu Parametresi Güncellendi']);
+        }else{return response()->json(['success'=>false,'message'=>'Sorgu Parametresi Güncellenemedi']);}
+    }
     public function destroy($id){}
 
 }
