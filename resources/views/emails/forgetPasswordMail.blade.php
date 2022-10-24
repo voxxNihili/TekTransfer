efc22043493852d837b19377f8d8f5cf4f4cdfab
Hakan küçüksert <hakan.kucuksert@teksenbilisim.com>
2022-10-24 18:06:59 +0300

    şifremi unuttum apisi ve maili yapıldı

---
+++ resources/views/emails/forgetPasswordMail.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Mail</title>
+</head>
+<body>
+ 
+ <h1>{{ $details['title'] }}</h1>
+ <p>Sayın, {{ $details['name'] }}<br>{{ $details['body'] }}</p>
+ <p>Şifreniz : ,<b>{{ $details['password'] }}</b></p>
+ 
+</body>
+</html> 
