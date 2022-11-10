05007826670880ab435549c0f764cecbbc12d91f
Hakan küçüksert <hakan.kucuksert@teksenbilisim.com>
2022-11-10 14:55:56 +0300

    fix

--- app/Models/ProductMonthRange.php
+++ app/Models/ProductMonthRange.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ProductMonthNumber extends Model
+class ProductMonthRange extends Model
 {
     use HasFactory;
     protected $guarded  = [];
