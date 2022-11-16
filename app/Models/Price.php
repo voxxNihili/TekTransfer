c6a025fa216511ad6b370b12bda05f8b8ec59611
Hakan küçüksert <hakan.kucuksert@teksenbilisim.com>
2022-11-16 13:56:16 +0300

    fiyatlandırma eklendi

---
+++ app/Models/Price.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductPrice extends Model
+{
+    use HasFactory;
+    protected $fillable = ['productId','userLimitId','monthLimitId','price'];
+}
