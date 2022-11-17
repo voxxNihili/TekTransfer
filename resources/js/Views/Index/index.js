a21ab842ff2d9956961d7b0cf616d6b7b3b6b4c6
alpergurpinar <4valerian@gmail.com>
2022-11-17 13:47:51 +0300

    loading animation fix

--- resources/js/Views/Index/index.js
+++ resources/js/Views/Index/index.js
@@ -8,29 +8,29 @@ import HomeLayout from "../../Components/Layout/homeLayout";
 // import Products from "../../Components/src/LandingPageProducts";
 import ProductTabs from "../../Components/src/ProductTabs";
 import { Box } from "@mui/material";
-import Loading from "/assets/loadingAlt.gif";
+import Loading from "/assets/loading.gif";
 
 const Home = (props) => {
     const [errors, setErrors] = useState([]);
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        if (loading)
-            return (
-                <Box
-                    component="img"
-                    src={Loading}
-                    sx={{
-                        height: "100%",
-                        width: "100%",
-                        // maxHeight: { xs: 233, md: 167 },
-                        // maxWidth: { xs: 350, md: 250 },
-                    }}
-                    alt="loading..."
-                />
-            );
-    }, [loading]);
+    // useEffect(() => {
+    //     if (loading)
+    //         return (
+    //             <Box
+    //                 component="img"
+    //                 src={Loading}
+    //                 sx={{
+    //                     height: "100%",
+    //                     width: "100%",
+    //                     // maxHeight: { xs: 233, md: 167 },
+    //                     // maxWidth: { xs: 350, md: 250 },
+    //                 }}
+    //                 alt="loading..."
+    //             />
+    //         );
+    // }, [loading]);
 
     return (
         <HomeLayout>
