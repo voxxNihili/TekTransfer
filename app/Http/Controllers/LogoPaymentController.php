666a0dac43bd701b0759837fcdcf778603e6d1e8
Furkan ünsal <furkan.unsal@teksenbilisim.com>
2023-01-02 22:08:08 +0000

    Updated LogoPaymentController.php

--- app/Http/Controllers/LogoPaymentController.php
+++ app/Http/Controllers/LogoPaymentController.php
@@ -72,7 +72,7 @@ class LogoPaymentController extends Controller
                 'message'=>'Tahsilat aktarıldı.'
             ],200);
         }else {
-            dd($paymentRequest->response_message);
+            dd(str_contains($paymentRequest->response_message, '(508)'));
             return response()->json([
                 'success'=>false,
                 'returnMessage'=>$response->getBody()->getContents(),
