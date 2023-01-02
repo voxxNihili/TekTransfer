b281f37e5732d165df53a27146d5792c27d35e77
Furkan ünsal <furkan.unsal@teksenbilisim.com>
2023-01-02 22:32:38 +0000

    Updated LogoPaymentController.php

--- app/Http/Controllers/LogoPaymentController.php
+++ app/Http/Controllers/LogoPaymentController.php
@@ -94,7 +94,7 @@ class LogoPaymentController extends Controller
                 $currentParams['TAX_OFFICE'] = $request->TaxAuthority ? $request->TaxAuthority :" ";
                 $currentParams['COMPANY_ID'] = $request->companyId ? $request->companyId :" ";
                      $responseCurrent = logoCurrent::currentPostData($currentParams);
-            dd($responseCurrent);
+            dd($responseCurrent->getStatusCode());
             if($responseCurrent->getStatusCode() == 200){
                 $this->payment($request);
             }else {
